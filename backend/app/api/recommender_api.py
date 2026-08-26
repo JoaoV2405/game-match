9c0f5b9ecd62bbc31e3122d451e3fce7b1c4afde
JoaoV2405 <mendoncajoao2323@gmail.com>
2026-08-26 09:31:08 -0300

    feat(api): adição de rota health chore:  mudança de variavel FRONTEND_URL

--- backend/app/api/recommender_api.py
+++ backend/app/api/recommender_api.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI
 from .dependencies import lifespan
 from .games import router
+from .health import health_router
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -10,16 +11,20 @@ app = FastAPI(
     title="Game Matcher API",
     lifespan=lifespan,
 )
-FRONTEND_URL = os.getenv("FRONTEND_URL") 
-
+FRONTEND_URLS = os.getenv("FRONTEND_URLS", "").split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        FRONTEND_URL
+        FRONTEND_URLS
     ],
+    allow_origin_regex=(
+        r"^https://game-match-nu.*\.vercel\.app$"
+        r"|^http://localhost:\d+$"
+    ),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(health_router)
 app.include_router(router)
